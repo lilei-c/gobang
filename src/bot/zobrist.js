637c8009a6d7c0ed15f17ed13f7ed46ff46975be
unknown <notsofunnyha@qq.com>
2023-02-20 22:15:35 +0800

    Zobrist随机值长度改为31

--- src/bot/zobrist.js
+++ src/bot/zobrist.js
@@ -4,7 +4,7 @@ import { arrayN } from './support'
 class Zobrist {
   constructor({ size, randFn }) {
     // js 异或运算最多32位 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Bitwise_XOR
-    if (!randFn) randFn = () => random.int(0, 2 ** 30)
+    if (!randFn) randFn = () => random.int(0, 2 ** 31)
     this.max = arrayN(size).map((_) => arrayN(size).map(randFn))
     this.min = arrayN(size).map((_) => arrayN(size).map(randFn))
     this.code = randFn()
